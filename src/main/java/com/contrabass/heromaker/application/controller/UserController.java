f576c73d256f64c55bad64ab07f981570f482ff3
manje cho <whakswp123@naver.com>
2023-06-23 00:11:52 +0900

    mybatis 초기 설정

--- src/main/java/com/contrabass/heromaker/application/controller/UserController.java
+++ src/main/java/com/contrabass/heromaker/application/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.contrabass.heromaker.application.controller;
 
+import com.contrabass.heromaker.application.dto.User;
 import com.contrabass.heromaker.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -10,16 +16,16 @@ public class UserController {
     public UserService userService;
 
 
-//    @RequestMapping(value = "/home", method = RequestMethod.GET)
-//    public ModelAndView goHome() {
-//        ModelAndView mav = new ModelAndView();
-//        System.out.println("zzz");
-//        List<User> userList = userService.getUserList();
-//        System.out.println("userList = " + userList);
-//
-//        mav.addObject("userList", userList);
-//        mav.setViewName("content/home.html");
-//
-//        return mav;
-//    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView goHome() {
+        ModelAndView mav = new ModelAndView();
+
+        List<User> userList = userService.getUserList();
+        System.out.println("userList = " + userList);
+
+        mav.addObject("userList", userList);
+        mav.setViewName("content/home.html");
+
+        return mav;
+    }
 }
