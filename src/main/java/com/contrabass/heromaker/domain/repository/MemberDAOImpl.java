b0cf2fc4dfd9f2eb2e6da2a8b30213c2f7cf11cb
김대철 <horry10@naver.com>
2023-06-26 15:04:42 +0900

    Login 페이지 구현

--- src/main/java/com/contrabass/heromaker/domain/repository/MemberDAOImpl.java
+++ src/main/java/com/contrabass/heromaker/domain/repository/MemberDAOImpl.java
@@ -1,4 +1,19 @@
 package com.contrabass.heromaker.domain.repository;
 
-public class MemberDAOImpl {
+import com.contrabass.heromaker.application.dto.UserDTO;
+import com.contrabass.heromaker.domain.dao.MemberDAO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+
+    @Autowired
+    SqlSession sqlSession; // SqlSession 의존관계 주입
+
+    @Override
+    public String loginCheck(UserDTO dto){
+        return sqlSession.selectOne("member.login_check",dto);
+    }
 }
