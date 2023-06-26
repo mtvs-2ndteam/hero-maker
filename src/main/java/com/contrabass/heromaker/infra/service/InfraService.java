5cfddad89a71653fdbbec9efc18b457f5551b17f
김대철 <horry10@naver.com>
2023-06-26 14:30:15 +0900

    Login 페이지 구현

--- src/main/java/com/contrabass/heromaker/infra/service/InfraService.java
+++ src/main/java/com/contrabass/heromaker/infra/service/InfraService.java
@@ -1,4 +1,34 @@
 package com.contrabass.heromaker.infra.service;
 
+import com.contrabass.heromaker.application.dto.UserDTO;
+import com.contrabass.heromaker.domain.entity.User;
+import com.contrabass.heromaker.domain.repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfraService {
+    private final UserMapper userMapper;
+    @Autowired
+    public UserService(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public List<User> findAllMembers() {
+
+        List<User> members = userMapper.getUserList();
+
+        List<UserDTO> memberDTOs = new ArrayList<>();
+        for(User member : members) {
+            memberDTOs.add(changeToDTO(member));
+        }
+
+        System.out.println("findAllMembers Service method call..");
+
+        return memberDTOs;
+    }
+
+
+
 }
