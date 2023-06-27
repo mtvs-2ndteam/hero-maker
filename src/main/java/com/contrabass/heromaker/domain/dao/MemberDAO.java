38a038e748b079bf87bb89f5925ba5fdb906384e
김대철 <horry10@naver.com>
2023-06-27 16:52:33 +0900

    Login 페이지 구현

--- src/main/java/com/contrabass/heromaker/domain/dao/MemberDAO.java
+++
@@ -1,7 +0,0 @@
-package com.contrabass.heromaker.domain.dao;
-
-import com.contrabass.heromaker.application.dto.UserDTO;
-
-public interface MemberDAO {
-    public String loginCheck(UserDTO dto);
-}
