3bd2aa0459d7366d62fe640af0a99bddea443887
manje cho <whakswp123@naver.com>
2023-06-27 20:27:23 +0900

    테스트 수정 (#9)

--- src/test/java/com/contrabass/heromaker/HeroMakerUserInsertTest.java
+++ src/test/java/com/contrabass/heromaker/HeroMakerUserInsertTest.java
@@ -1,11 +1,7 @@
 package com.contrabass.heromaker;
 
-import com.contrabass.heromaker.application.dto.UserDTO;
 import com.contrabass.heromaker.application.service.UserService;
 import com.contrabass.heromaker.infra.repository.UserInfraRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,28 +25,28 @@ public class HeroMakerUserInsertTest {
 //        Assertions.assertEquals(1,userService.insertUser(userDTO));
 //    }
 
-    @DisplayName("데이터 조회  notNull 테스트")
-    @Test
-    void insertTest2() {
-        UserDTO userDTO = new UserDTO(0, "test1", "test1", "test1", "test1", "test1");
-
-        Assertions.assertNotNull(userService.insertUser(userDTO));
-    }
-
-    @DisplayName("데이터 조회 테스트")
-    @Test
-    void selectTest() {
-        int userNo = 1;
-        UserDTO userDTO = new UserDTO(1, "test1", "test1", "test1", "test1", "test1");
-
-        Assertions.assertNotNull(userService.getUser(1)); //받아온 값이 null인지 비교
-
-        Assertions.assertEquals(userDTO.getUserNo(), userService.getUser(1).getUserNo()); //받아온 값과 db의 있는 값이 같은지 비교
-
-        org.assertj.core.api.Assertions.assertThat(userDTO).isInstanceOf(UserDTO.class); //받아온 데이터가 UserDTO 타입인지 확인
-
-        org.assertj.core.api.Assertions.assertThat(userDTO).usingRecursiveComparison().isEqualTo(userDTO);  //받아온 데이터가 테스트 데이터와 같은지 확인
-    }
+//    @DisplayName("데이터 조회  notNull 테스트")
+//    @Test
+//    void insertTest2() {
+//        UserDTO userDTO = new UserDTO(0, "test1", "test1", "test1", "test1", "test1");
+//
+//        Assertions.assertNotNull(userService.insertUser(userDTO));
+//    }
+//
+//    @DisplayName("데이터 조회 테스트")
+//    @Test
+//    void selectTest() {
+//        int userNo = 1;
+//        UserDTO userDTO = new UserDTO(1, "test1", "test1", "test1", "test1", "test1");
+//
+//        Assertions.assertNotNull(userService.getUser(1)); //받아온 값이 null인지 비교
+//
+//        Assertions.assertEquals(userDTO.getUserNo(), userService.getUser(1).getUserNo()); //받아온 값과 db의 있는 값이 같은지 비교
+//
+//        org.assertj.core.api.Assertions.assertThat(userDTO).isInstanceOf(UserDTO.class); //받아온 데이터가 UserDTO 타입인지 확인
+//
+//        org.assertj.core.api.Assertions.assertThat(userDTO).usingRecursiveComparison().isEqualTo(userDTO);  //받아온 데이터가 테스트 데이터와 같은지 확인
+//    }
 
 
 }
