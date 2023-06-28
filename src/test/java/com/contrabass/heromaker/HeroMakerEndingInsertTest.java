e5f677ac6f0872f7fe6838710e50b672b6b7eb81
youyeul lee <yuyeul301@gmail.com>
2023-06-28 14:12:30 +0900

    엔딩 DTO 저장 및 엔딩 ending데이터 HashSet으로 조회 테스트 주석 #31

--- src/test/java/com/contrabass/heromaker/HeroMakerEndingInsertTest.java
+++ src/test/java/com/contrabass/heromaker/HeroMakerEndingInsertTest.java
@@ -24,52 +24,52 @@ public class HeroMakerEndingInsertTest {
         this.userDomainService= userDomainService;
     }
 
-    @DisplayName("데이터 삽입 테스트")
-    @Test
-    void insertTest1() {
-        EndingDTO endingDTO=new EndingDTO();
+//    @DisplayName("데이터 삽입 테스트")
+//    @Test
+//    void insertTest1() {
+//        EndingDTO endingDTO=new EndingDTO();
+//
+//        endingDTO.setUserNo(1);
+//        endingDTO.setEnding("1");
+//        endingDTO.setStatus("Y");
+//
+//        Assertions.assertEquals(1,endingCheckService.insertEnding(endingDTO));
+//    }
 
-        endingDTO.setUserNo(1);
-        endingDTO.setEnding("1");
-        endingDTO.setStatus("Y");
+//    @DisplayName("데이터 삽입  notNull 테스트")
+//    @Test
+//    void insertTest2() {
+//
+//        EndingDTO endingDTO=new EndingDTO();
+//
+//        endingDTO.setUserNo(3);
+//        endingDTO.setEnding("4");
+//        endingDTO.setStatus("Y");
+//
+//        Assertions.assertNotNull(endingCheckService.insertEnding(endingDTO));
+//    }
 
-        Assertions.assertEquals(1,endingCheckService.insertEnding(endingDTO));
-    }
-
-    @DisplayName("데이터 삽입  notNull 테스트")
-    @Test
-    void insertTest2() {
-
-        EndingDTO endingDTO=new EndingDTO();
-
-        endingDTO.setUserNo(3);
-        endingDTO.setEnding("4");
-        endingDTO.setStatus("Y");
-
-        Assertions.assertNotNull(endingCheckService.insertEnding(endingDTO));
-    }
-
-    @DisplayName("데이터 조회  notNull 테스트")
-    @Test
-    void insertTest3() {
-
-        int userNo=1;
+//    @DisplayName("데이터 조회  notNull 테스트")
+//    @Test
+//    void insertTest3() {
+//
+//        int userNo=1;
 //        HashSet<String> set=endingCheckService.collectEnding(userNo);
 //        Iterator iterator= set.iterator();
 //        while (iterator.hasNext()){
 //            System.out.println(iterator.next());
 //        }
-        Assertions.assertNotNull(endingCheckService.collectEnding(userNo));
-    }
+//        Assertions.assertNotNull(endingCheckService.collectEnding(userNo));
+//    }
 
-    @DisplayName("데이터 조회 데이터 일치 테스트")
-    @Test
-    void insertTest4() {
-
-        int userNo=3;
-        String testDBEnding="4";
-        HashSet<String> set=endingCheckService.collectEnding(userNo);
-
-        Assertions.assertTrue(set.contains(testDBEnding));
-    }
+//    @DisplayName("데이터 조회 데이터 일치 테스트")
+//    @Test
+//    void insertTest4() {
+//
+//        int userNo=3;
+//        String testDBEnding="4";
+//        HashSet<String> set=endingCheckService.collectEnding(userNo);
+//
+//        Assertions.assertTrue(set.contains(testDBEnding));
+//    }
 }
